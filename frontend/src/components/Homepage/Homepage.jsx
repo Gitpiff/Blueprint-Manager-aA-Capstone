565be8db67d858996434eeafc19ee7b7f93f226d
Victor <64720348+Gitpiff@users.noreply.github.com>
2024-08-09 22:54:05 +0100

    Signup Form done, initial logout button commit

--- frontend/src/components/Homepage/Homepage.jsx
+++ frontend/src/components/Homepage/Homepage.jsx
@@ -1,10 +1,21 @@
 import { useSelector } from 'react-redux';
 import Navbar from "../Navbar/Navbar";
+import { useDispatch } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import Footer from '../Footer/Footer';
+import * as sessionActions from '../../store/session';
 
 function Homepage() {
+    const dispatch = useDispatch();
     const sessionUser = useSelector((state) => state.session.user);
+
+    const logout = (e) => {
+        e.preventDefault();
+
+        dispatch(sessionActions.logout());
+    
+        return <Navigate to="/" />;
+    };
 
     if (!sessionUser) {
         return <Navigate to="/" />;
@@ -13,6 +24,7 @@ function Homepage() {
     return (
         <div className="mainContainer">
             <Navbar />
+            <button onClick={logout}>Logout</button>
             <h1>Homepage</h1>
             <Footer />
         </div>
--- frontend/src/components/SignupFormPage/SignupFormPage.jsx
+++ frontend/src/components/SignupFormPage/SignupFormPage.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import * as sessionActions from '../../store/session';
+import './SignUpForm.css'
 
 const SignUpFormPage = () => {
   const dispatch = useDispatch();
@@ -47,98 +48,101 @@ const SignUpFormPage = () => {
 
   return (
     <>
-      <h1>Sign Up</h1>
-      <form onSubmit={handleSubmit}>
+    <div style={{ backgroundColor: '#001f3f' }} className='signup-modal'>
+        <h1 className='login-title'>Sign Up</h1>
+        <form className='form' onSubmit={handleSubmit}>
         <label>
-          Email
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </label>
-        {errors.email && <p>{errors.email}</p>}
-        
-        <label>
-          Username
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
-        </label>
-        {errors.username && <p>{errors.username}</p>}
-        
-        <label>
-          First Name
-          <input
-            type="text"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-            required
-          />
-        </label>
-        {errors.firstName && <p>{errors.firstName}</p>}
-        
-        <label>
-          Last Name
-          <input
-            type="text"
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-            required
-          />
-        </label>
-        {errors.lastName && <p>{errors.lastName}</p>}
-        
-        <label>
-          Company Name
-          <input
-            type="text"
-            value={companyName}
-            onChange={(e) => setCompanyName(e.target.value)}
-            required
-          />
-        </label>
-        {errors.companyName && <p>{errors.companyName}</p>}
-        
-        <label>
-          Industry Sector
-          <input
-            type="text"
-            value={industrySector}
-            onChange={(e) => setIndustrySector(e.target.value)}
-            required
-          />
-        </label>
-        {errors.industrySector && <p>{errors.industrySector}</p>}
-        
-        <label>
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-        {errors.password && <p>{errors.password}</p>}
-        
-        <label>
-          Confirm Password
-          <input
-            type="password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            required
-          />
-        </label>
-        {errors.confirmPassword && <p>{errors.confirmPassword}</p>}
-        
-        <button type="submit">Sign Up</button>
-      </form>
+            First Name
+            <input
+              type="text"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              required
+            />
+          </label>
+          {errors.firstName && <p className='errors'>{errors.firstName}</p>}
+
+          <label>
+            Last Name
+            <input
+              type="text"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+              required
+            />
+          </label>
+          {errors.lastName && <p className='errors'>{errors.lastName}</p>}
+
+          <label>
+            Username
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </label>
+          {errors.username && <p className='errors'>{errors.username}</p>}
+          
+          <label>
+            Email
+            <input
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </label>
+          {errors.email && <p className='errors'>{errors.email}</p>}
+          
+          <label>
+            Company Name
+            <input
+              type="text"
+              value={companyName}
+              onChange={(e) => setCompanyName(e.target.value)}
+              required
+            />
+          </label>
+          {errors.companyName && <p className='errors'>{errors.companyName}</p>}
+          
+          <label>
+            Industry Sector
+            <input
+              type="text"
+              value={industrySector}
+              onChange={(e) => setIndustrySector(e.target.value)}
+              required
+            />
+          </label>
+          {errors.industrySector && <p className='errors'>{errors.industrySector}</p>}
+          
+          <label>
+            Password
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </label>
+          {errors.password && <p className='errors'>{errors.password}</p>}
+          
+          <label>
+            Confirm Password
+            <input
+              type="password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              required
+            />
+          </label>
+          {errors.confirmPassword && <p className='errors'>{errors.confirmPassword}</p>}
+          <div className='signup-btns'>
+            <button type="submit">Sign Up</button>
+          </div>
+        </form>
+    </div>
     </>
   );
 };
