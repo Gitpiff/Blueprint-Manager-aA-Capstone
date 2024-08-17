565cb6f6cfe5e85b912a603fa201d212d90cdee9
Victor <64720348+Gitpiff@users.noreply.github.com>
2024-08-17 20:23:59 +0100

    delete project modal css done

--- frontend/src/components/DeleteProject/DeleteProject.jsx
+++ frontend/src/components/DeleteProject/DeleteProject.jsx
@@ -2,6 +2,7 @@ import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal"
 import { deleteProject } from "../../store/project";
 import { Navigate } from "react-router-dom";
+import './DeleteProject.css'
 
 const DeleteProject = ({ projectId }) => {
     const { closeModal } = useModal();
@@ -16,7 +17,7 @@ const DeleteProject = ({ projectId }) => {
     return (
         <div className="delete">
             <h1>Confirm Delete</h1>
-            <p>Are you sure you want to remove this Project?</p>
+            <h2>Are you sure you want to remove this Project?</h2>
             <div className="confirm-delete">
                 <button className="yes-delete" onClick={yes}>Yes (Delete Project)</button>
                 <button className="no-delete" onClick={closeModal}>No (Keep Project)</button>
