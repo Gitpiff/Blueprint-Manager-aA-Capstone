b29a4741afaaaab5975d0bab1bb237fe3b7a34b2
Victor <64720348+Gitpiff@users.noreply.github.com>
2024-09-16 00:05:21 +0100

    Update index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -10,7 +10,7 @@ if (process.env.NODE_ENV === 'production') {
   router.get('/', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
     res.sendFile(
-        path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
+        path.resolve(__dirname, '../../frontend', 'dist/assets/', 'index.html')
     );
   });
 
