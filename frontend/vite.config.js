b03f7d35266d85f008c78b92cbf99e84eae51d9e
Victor <64720348+Gitpiff@users.noreply.github.com>
2024-09-17 22:26:40 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,24 +1,32 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
+import commonjs from 'vite-plugin-commonjs';
+import vitePluginRequire from "vite-plugin-require";
+import requireTransform from 'vite-plugin-require-transform';
 
 // https://vitejs.dev/config/
+
 export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
+    commonjs(),
     eslint({
       lintOnStart: true,
       failOnError: mode === "production"
-    })
+    }),
+    vitePluginRequire.default(),
+    requireTransform({}),
   ],
   server: {
     proxy: {
       '/api': 'http://localhost:8000'
     },
+    open: true
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true
+    }
   }
-  // To automatically open the app in the browser whenever the server starts,
-  // uncomment the following lines:
-  // server: {
-  //   open: true
-  // }
 }));
