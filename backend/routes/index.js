c8eab0a7a8574c20e855c405f9a333f7034e42af
Victor <64720348+Gitpiff@users.noreply.github.com>
2024-09-30 22:32:35 +0100

    Update index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === 'production') {
   });
 
   // Serve the static assets in the frontend's build folder
-  router.use(express.static(path.resolve("../frontend/dist/assets")));
+  router.use(express.static(path.resolve("../frontend/dist")));
   // frontend/dist
 
   // router.use(express.static(path.join(__dirname, 'build')));
