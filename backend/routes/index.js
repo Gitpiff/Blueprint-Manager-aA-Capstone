4f0b2df42d1b5e988fe8ad467bb7e03b12f768a5
Victor <64720348+Gitpiff@users.noreply.github.com>
2024-10-01 23:03:02 +0100

    Update index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,3 +1,4 @@
+// backend/routes/index.js
 const express = require('express');
 const router = express.Router();
 const apiRouter = require('./api');
@@ -18,11 +19,6 @@ if (process.env.NODE_ENV === 'production') {
 
   // Serve the static assets in the frontend's build folder
   router.use(express.static(path.resolve("../frontend/dist")));
-  // frontend/dist
-
-  // router.use(express.static(path.join(__dirname, 'build')));
-  // router.use('/assets', express.static(path.join(__dirname, 'public/assets')));
-
 
   // Serve the frontend's index.html file at all other routes NOT starting with /api
   router.get(/^(?!\/?api).*/, (req, res) => {
