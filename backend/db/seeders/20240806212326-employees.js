2868366c9a36e678f86e61680127df37b11286d4
Victor <64720348+Gitpiff@users.noreply.github.com>
2024-10-14 21:46:33 +0100

    updated employee seeder

--- backend/db/seeders/20240806212326-employees.js
+++ backend/db/seeders/20240806212326-employees.js
@@ -1,5 +1,5 @@
- 'use strict';
-const Employee = require('../models/employee')
+'use strict';
+const Employee = require('../models/employee');
 
 const demoEmployees = [
   {
@@ -24,37 +24,37 @@ const demoEmployees = [
     picture: 'https://i0.wp.com/construct-ed.com/wp-content/uploads/2022/10/construction-foreman-job-description.jpg?fit=933%2C1000&ssl=1',
     projectId: 1,
   }
-]
+];
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    const options = { tableName: 'Employees'};
+    const options = { tableName: 'Employees' };
 
     if (process.env.NODE_ENV === 'production') {
       options.schema = process.env.SCHEMA;  
     }
 
     try {
-      await Employee.bulkCreate(demoEmployees, { validate: true })
+      return await queryInterface.bulkInsert(options, demoEmployees, { validate: true });
     } catch (error) {
-     console.log(`Error Creating Employees: `, error)
+      console.error(`Error Creating Employees: ${error.message}`, error);
     }
   },
 
   async down (queryInterface, Sequelize) {
-    try {
-      const options = { tableName: 'Employees'};
-      const Op = Sequelize.Op;
+    const options = { tableName: 'Employees' };
+    const Op = Sequelize.Op;
 
-      if (process.env.NODE_ENV === 'production') {
-        options.schema = process.env.SCHEMA;  
-      }
-      
-      await queryInterface.bulkDelete(options, {
-       [Op.or]: demoEmployees
-      }, {}); 
+    if (process.env.NODE_ENV === 'production') {
+      options.schema = process.env.SCHEMA;  
+    }
+
+    try {
+      return await queryInterface.bulkDelete(options, {
+        [Op.or]: demoEmployees.map(employee => ({ email: employee.email }))
+      }, {});
     } catch (error) {
-      console.error('Error Deleting Employees', error);
+      console.error(`Error Deleting Employees: ${error.message}`, error);
     }
   }
 };
